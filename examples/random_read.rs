b01aad64338f2ef6b05395424194755bb8aab5f7
xutianyi <xutianyi1999@live.com>
2023-11-08 10:08:14 +0800

    u

--- examples/random_read.rs
+++ examples/random_read.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::Instant;
 use rand::Rng;
 use batch_access::{batch_read, Chunk};
 
@@ -14,12 +15,18 @@ pub fn main() {
     let metadata = file_path.metadata().expect("file does not exist");
     let file_len = metadata.len();
 
+    println!("alloc temp memory");
+
     let mut chunks = vec![Chunk { data: Vec::with_capacity(32), pos: 0 }; num];
     let mut rng = rand::thread_rng();
 
+    println!("gen random pos");
     for x in &mut chunks {
         x.pos = rng.gen_range(0..(file_len as usize - 31));
     }
 
+    println!("start batch read");
+    let t = Instant::now();
     batch_read(file_path, &mut chunks).unwrap();
+    println!("end batch read, use {:?}", t.elapsed())
 }
